d06f4ba75b71a6c9e184603fe51d248b4b5d62cb
not found <user@not.found>
2024-10-08 15:08:06 +0300

    first commit

---
+++ index.search.js
@@ -0,0 +1,90 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "1. Ayet 2. Ayet 3. Ayet 4. Ayet 5. Ayet 6. Ayet 7. Ayet",
+    "description": "1. Ayet 2. Ayet 3. Ayet 4. Ayet 5. Ayet 6. Ayet 7. Ayet",
+    "tags": [],
+    "title": "Fatiha",
+    "uri": "/fatiha-suresi/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça بِسْمِ اللَّهِ اَلرَّحْمٰنِ الرَّحِيمِ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça بِسْمِ اللَّهِ اَلرَّحْمٰنِ الرَّحِيمِ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 1. Ayet",
+    "uri": "/fatiha-suresi/1/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça اَلْحَمْدُ لِلّٰهِ رَبِّ الْعَالَم۪ينَۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça اَلْحَمْدُ لِلّٰهِ رَبِّ الْعَالَم۪ينَۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 2. Ayet",
+    "uri": "/fatiha-suresi/2/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça اَلرَّحْمٰنِ الرَّح۪يمِۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça اَلرَّحْمٰنِ الرَّح۪يمِۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 3. Ayet",
+    "uri": "/fatiha-suresi/3/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça مَالِكِ يَوْمِ الدّ۪ينِۜ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça مَالِكِ يَوْمِ الدّ۪ينِۜ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 4. Ayet",
+    "uri": "/fatiha-suresi/4/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَع۪ينُۜ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَع۪ينُۜ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 5. Ayet",
+    "uri": "/fatiha-suresi/5/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça اِهْدِنَا الصِّرَاطَ الْمُسْتَق۪يمَۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça اِهْدِنَا الصِّرَاطَ الْمُسْتَق۪يمَۙ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 6. Ayet",
+    "uri": "/fatiha-suresi/6/index.html"
+  },
+  {
+    "breadcrumb": "Fatiha",
+    "content": "Yazar Meal Arapça صِرَاطَ الَّذ۪ينَ اَنْعَمْتَ عَلَيْهِمْۙ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّٓالّ۪ينَ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "description": "Yazar Meal Arapça صِرَاطَ الَّذ۪ينَ اَنْعَمْتَ عَلَيْهِمْۙ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّٓالّ۪ينَ Abdulbaki Gölpınarlı Rahman ve rahim Allah adiyle Abdullah-Ahmet Akgül Rahman ve Rahim olan Allah’ın adıyla Abdullah Parlıyan Dünyada herkesi, ahirette sadece mü’minleri rahmetine alan Allah adına Ahmet Tekin Yaratan, yaşama kabiliyeti, gücü ve varlıklara işleyiş düzeni veren, koruyan, kontrol eden, âlemlerin, bütün varlıkların mürebbisi, sahibi Allah’a hamdolsun Ahmet Varol Hamd alemlerin Rabb’i olan Allah’adır Ali Bulaç 2,3,4. Hamd, Alemlerin Rabbi, Rahman, Rahim ve Din gününün maliki olan Allah’adır Ali Fikri Yavuz (Ezelden ebede kadar) bütün olmuş ve olacak hamd ve sena (övgü) tam ve kemaliyle âlemlerin (yegâne) yaratıcısı, besleyip kemale erdiricisi olan Allah’adır",
+    "tags": [],
+    "title": "Fatiha Suresi 7. Ayet",
+    "uri": "/fatiha-suresi/7/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Fatiha Suresi",
+    "description": "Fatiha Suresi",
+    "tags": [],
+    "title": "Türkçe Kuran Mealleri",
+    "uri": "/index.html"
+  }
+]
